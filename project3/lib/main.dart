d05e0bd314b87c13beab78af222ec556b51f97ee
Joe <emailforinterwebs.jt@gmail.com>
2025-04-14 17:28:38 -0400

    initial project framework created

--- project3/lib/main.dart
+++ project3/lib/main.dart
@@ -1,4 +1,27 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:developer';
+import 'package:http/http.dart' as http;
+import 'package:go_router/go_router.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+/// @author: Joe Trovato
+/// @version: 0.1.0
+/// @since: 2025-04-14
+/// 
+/// todo: everything
+/// 
+/// notes: none
+
+final GoRouter router = GoRouter(
+  initialLocation: "/",
+  routes: [
+    GoRoute(
+      path: "/",
+      builder: (context, state) => const MainPage(),
+    ),
+  ]
+);
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +32,29 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      routerConfig: router,
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hello World!'),
       ),
     );
   }
